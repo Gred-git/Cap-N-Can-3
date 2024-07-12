176e9c141da17b1f27a92f5539b6e97be19106fe
Gred-git <90420916+Gred-git@users.noreply.github.com>
2024-07-12 16:51:00 +0000

    site hosted ?

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Home from "./pages";
+import Search from './pages/Search';
+import Challenges from './pages/Challenges';
+import Forums from './pages/Forums';
+
+import Navbar from "./components/Navbar";
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/challenges" element={<Challenges />} />
+                <Route path="/forums" element={<Forums />} />
+            </Routes>
+        </Router>
+    );
 }
-
+ 
 export default App;
